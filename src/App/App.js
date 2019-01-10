d079d3fe6a9bf32ca241443a2f0430533d4fc7ff
Mary Rys <rys.mary@gmail.com>
2019-01-09 19:56:48 -0600

    authed state working

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import connection from '../helpers/data/connection';
+
+import Auth from '../components/Auth/Auth';
 import './App.scss';
 
+// import authRequests from '../helpers/data/authRequests';
+
 class App extends React.Component {
+  state ={
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    const { authed } = this.state;
+
+    if (!authed) {
+      return (
+        <div className="App">
+          {/* <MyNavBar isAuthed={authed} logoutClickEvent={logoutClickEvent}/> */}
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <button className='btn btn-danger'>HELP ME</button>
-        <Button
-            tag="a"
-            color="success"
-            size="large"
-            href="http://reactstrap.github.io"
-            target="_blank"
-          >
-            View Reactstrap Docs
-        </Button>
+      You are authenticated.
       </div>
     );
   }
